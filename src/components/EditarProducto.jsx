4cf320c94a027a053e5b168525fcdfc13ede890f
Rafa Escalnte <rafaescalnte@MacBook-Pro-de-Lol.local>
2021-10-25 05:33:37 -0700

    forms

--- src/components/EditarProducto.jsx
+++ src/components/EditarProducto.jsx
@@ -1,7 +1,30 @@
 const EditarProducto = () => {
     return (
         <div>
-            <h1>editar</h1>
+            <form>
+                            <div>
+                                <label>Nombre Producto</label>
+                                <input
+                                    type="text"
+                                    name="nombre"
+                                  /*   value={nombre}
+                                    onChange={e => guardarNombre(e.target.value)} */
+                                />
+                            </div>
+
+                            <div>
+                                <label>Precio Producto</label>
+                                <input
+                                    type="number"
+                                    name="precio"
+                                  /*   value={precio}
+                                    onChange={e =>  guardarPrecio( Number(e.target.value) )} */
+                                />
+                            </div>
+
+                            <button type="submit">Edit</button>
+                </form>
+
         </div>
     )
 }
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -4,7 +4,24 @@
 const Productos = () => {
     return (
         <div>
-          <h1>productos</h1>  
+         <h2>Listado de Productos</h2>
+
+{/* { error ? <p className="error">Hubo un error</p> : null }
+
+{ cargando ? <p>Cargando....</p> : null } */}
+
+<table>
+    <thead>
+         <tr>
+             <th scope="col">Nombre</th>
+             <th scope="col">Precio</th>
+             <th scope="col">Acciones</th>
+         </tr>
+    </thead>
+    <tbody>
+      
+    </tbody>
+</table> 
         </div>
     )
 }
