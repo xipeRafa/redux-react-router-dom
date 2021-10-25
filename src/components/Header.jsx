bc77ff28f4954d78d74276a1bc97886f520652dc
Rafa Escalnte <rafaescalnte@MacBook-Pro-de-Lol.local>
2021-10-25 05:17:28 -0700

    LINK

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
+
+
 const Header = () => {
     return (
-        <div>
-            <h1>header</h1>
-        </div>
+        <nav>
+            <Link to={'/'} >
+                Prductos
+            </Link> 
+         
+            <Link to={"/productos/nuevo"}>
+                Agregar Producto &#43;
+            </Link>
+    </nav>
     )
 }
 
