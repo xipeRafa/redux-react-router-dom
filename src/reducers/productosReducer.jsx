0c055e571a90aea367381ff7ebb210dcbb5a73ed
rafael <rafaelsistem87@gmail.com>
2021-10-25 22:18:51 -0700

    nuevoProducto

---
+++ src/reducers/productosReducer.jsx
@@ -0,0 +1,30 @@
+import {
+      AGREGAR_PRODUCTO,
+      AGREGAR_PRODUCTO_EXITO,
+      AGREGAR_PRODUCTO_ERROR,
+      COMENZAR_DESCARGA_PRODUCTOS,
+      DESCARGA_PRODUCTOS_EXITO,
+      DESCARGA_PRODUCTOS_ERROR,
+      OBTENER_PRODUCTO_ELIMINAR,
+      PRODUCTO_ELIMINADO_EXITO,
+      PRODUCTO_ELIMINADO_ERROR,
+      OBTENER_PRODUCTO_EDITAR,
+      PRODUCTO_EDITADO_EXITO,
+      PRODUCTO_EDITADO_ERROR
+  } from '../types';
+
+//cada reeducer tiene su propio state
+const initialState = {
+      productos: [],
+      error: null,
+      loading: false, 
+      productoeliminar: null,
+      productoeditar: null
+  }
+  
+  export default function(state = initialState, action) {
+      switch(action.type) {
+            default:
+                  return state;
+      }
+}
