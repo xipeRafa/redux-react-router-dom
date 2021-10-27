e7df63a02380d22c32ec152c24dc1be8b32dfabd
rafael <rafaelsistem87@gmail.com>
2021-10-27 01:44:15 -0700

    ident 2

--- src/reducers/productosReducer.jsx
+++ src/reducers/productosReducer.jsx
@@ -1,17 +1,7 @@
-import {
-      AGREGAR_PRODUCTO,
-      AGREGAR_PRODUCTO_EXITO,
-      AGREGAR_PRODUCTO_ERROR,
-      COMENZAR_DESCARGA_PRODUCTOS,
-      DESCARGA_PRODUCTOS_EXITO,
-      DESCARGA_PRODUCTOS_ERROR,
-      OBTENER_PRODUCTO_ELIMINAR,
-      PRODUCTO_ELIMINADO_EXITO,
-      PRODUCTO_ELIMINADO_ERROR,
-      OBTENER_PRODUCTO_EDITAR,
-      PRODUCTO_EDITADO_EXITO,
-      PRODUCTO_EDITADO_ERROR
-  } from '../types';
+import {            AGREGAR_PRODUCTO,    AGREGAR_PRODUCTO_EXITO,    AGREGAR_PRODUCTO_ERROR,
+         COMENZAR_DESCARGA_PRODUCTOS,  DESCARGA_PRODUCTOS_EXITO,  DESCARGA_PRODUCTOS_ERROR,
+           OBTENER_PRODUCTO_ELIMINAR,  PRODUCTO_ELIMINADO_EXITO,  PRODUCTO_ELIMINADO_ERROR,
+             OBTENER_PRODUCTO_EDITAR,    PRODUCTO_EDITADO_EXITO,    PRODUCTO_EDITADO_ERROR } from '../types';
 
 //cada reeducer tiene su propio state
 const initialState = {
@@ -55,9 +45,8 @@ const initialState = {
                       productos: state.productos.filter( producto => producto.id !== state.productoeliminar ),
                       productoeliminar: null
                   }
-                  
-            case  OBTENER_PRODUCTO_EDITAR:
-                  return { ...state, productoeditar: action.payload }
+
+            case  OBTENER_PRODUCTO_EDITAR: return { ...state, productoeditar: action.payload }
 
             case PRODUCTO_EDITADO_EXITO:
                   return {
